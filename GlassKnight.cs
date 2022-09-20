f211812279a23865b4d1e208df4be141c13bc118
MathisB <mathis.bicheyre@efrei.net>
2022-09-20 21:15:20 +0200

    template

---
+++ GlassKnight.cs
@@ -0,0 +1,39 @@
+﻿using Modding;
+using System;
+
+namespace GlassKnight
+{
+    public class GlassKnight : Mod
+    {
+        private static GlassKnight? _instance;
+
+        internal static GlassKnight Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new InvalidOperationException($"An instance of {nameof(GlassKnight)} was never constructed");
+                }
+                return _instance;
+            }
+        }
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
+
+        public GlassKnight() : base()
+        {
+            _instance = this;
+        }
+
+        // if you need preloads, you will need to implement GetPreloadNames and use the other signature of Initialize.
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            // put additional initialization logic here
+
+            Log("Initialized");
+        }
+    }
+}
